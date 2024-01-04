30813465a05c33c8e14b95cb22532e13398e89f2
vsiago <vsiago22@gmail.com>
2024-01-04 08:46:02 -0300

    Melhorias do payload

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -1,28 +1,27 @@
-const jwt = require('jsonwebtoken');
-const crypto = require('crypto');
+const jwt = require("jsonwebtoken");
+const crypto = require("crypto");
 
 function verificarToken(req, res, next) {
-    const tokenHeader = req.headers.authorization;
+  const tokenHeader = req.headers.authorization;
 
-    if(!tokenHeader) {
-        return res.status(401).json({ message: "Token não fornecido."});
+  if (!tokenHeader) {
+    return res.status(401).json({ message: "Token não fornecido." });
+  }
+
+  // Remova a parte "Bearer " do token
+  const token = tokenHeader.split(" ")[1];
+
+  // const chaveSecreta = crypto.randomBytes(32).toString('hex');
+
+  jwt.verify(token, "MinhaChaveSecreta", (err, decoded) => {
+    if (err) {
+      console.error(err);
+      return res.status(401).json({ message: "Token inválido." });
     }
 
-    // Remova a parte "Bearer " do token
-    const token = tokenHeader.split(' ')[1];
-
-    // const chaveSecreta = crypto.randomBytes(32).toString('hex');
-
-    jwt.verify(token, 'MinhaChaveSecreta', (err, decoded) => {
-        if(err) {
-            console.error(err);
-            return res.status(401).json({ message: "Token inválido." });
-        }
-
-
-        req.usuario = decoded;
-        next();
-    })
+    req.usuario = decoded;
+    next();
+  });
 }
 
 module.exports = verificarToken;
--- src/routes/loginRoute.js
+++ src/routes/loginRoute.js
@@ -1,51 +1,63 @@
-const express = require('express');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-const Cadastro = require('../models/cadastroModel');
+const express = require("express");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const Cadastro = require("../models/cadastroModel");
 
 const loginRouter = express.Router();
 
-loginRouter.post('/login', async (req, res) => {
-    const { email, password } = req.body;
+loginRouter.post("/login", async (req, res) => {
+  const { email, password } = req.body;
 
-    try {
+  try {
+    // Verifica se o usuário existe no banco de dados
+    const user = await Cadastro.findOne({ email });
 
-        // Verifica se o usuário existe no banco de dados
-        const user = await Cadastro.findOne({ email });
-
-        if(!user) {
-            return res.status(404).json({ message: "Usuário não encontrado."});
-        }
-
-        // Verificar se a senha está correta
-        const passwordMatch = await bcrypt.compare(password, user.password)
-
-        if(!passwordMatch) {
-            return res.status(401).json({ message: "Senha incorreta."})
-        }
-
-        // Gerar token JWT
-        const token = jwt.sign({ userId: user._id, email: user.email }, 'MinhaChaveSecreta', { expiresIn: '10m'})
-        
-        console.log(token)
-        // Login bem sucedido
-        res.status(200).json({ token })
-    } catch (error) {
-        console.error('Erro durante o login:', error.message);
-        res.status(500).json({ message: "Erro durante o login."});
+    if (!user) {
+      return res.status(404).json({ message: "Usuário não encontrado." });
     }
+
+    // Verificar se a senha está correta
+    const passwordMatch = await bcrypt.compare(password, user.password);
+
+    if (!passwordMatch) {
+      return res.status(401).json({ message: "Senha incorreta." });
+    }
+
+    // Login bem sucedido - retorna o objeto completo do usuário
+    res.status(200).json({
+      _id: user._id,
+      username: user.username,
+      email: user.email,
+      name: user.name,
+      // Adicione outros campos conforme necessário
+
+      token: jwt.sign(
+        {
+          userId: user._id,
+          username: user.username,
+          email: user.email,
+          name: user.name,
+        },
+        "MinhaChaveSecreta",
+        { expiresIn: "10m" }
+      ),
+    });
+  } catch (error) {
+    console.error("Erro durante o login:", error.message);
+    res.status(500).json({ message: "Erro durante o login." });
+  }
 });
 
-loginRouter.get('/listar-usuarios', async (req, res) => {
-    try {
-        // Certifique-se de que a rota só seja acessível por usuários autenticados (com um token válido)
-        const usuarios = await Cadastro.find({}, { password: 0 }); // O segundo argumento ({ password: 0 }) exclui a senha da resposta
+loginRouter.get("/listar-usuarios", async (req, res) => {
+  try {
+    // Certifique-se de que a rota só seja acessível por usuários autenticados (com um token válido)
+    const usuarios = await Cadastro.find({}, { password: 0 }); // O segundo argumento ({ password: 0 }) exclui a senha da resposta
 
-        res.status(200).json(usuarios);
-    } catch (error) {
-        console.error('Erro ao listar usuários:', error.message);
-        res.status(500).json({ message: "Erro ao listar usuários." });
-    }
-})
+    res.status(200).json(usuarios);
+  } catch (error) {
+    console.error("Erro ao listar usuários:", error.message);
+    res.status(500).json({ message: "Erro ao listar usuários." });
+  }
+});
 
 module.exports = loginRouter;
